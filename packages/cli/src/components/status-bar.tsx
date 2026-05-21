6dd7eb0417f5c83755f5e597e2c6cc4c88133cf2
Sankeerth Reddy <sankeerth@Sankeerths-MacBook-Pro-2.local>
2026-05-21 16:33:36 -0700

    status bar update

--- packages/cli/src/components/status-bar.tsx
+++ packages/cli/src/components/status-bar.tsx
@@ -1,13 +1,18 @@
 import { TextAttributes } from "@opentui/core";
 
 export function StatusBar() {
-    return (
-        <box flexDirection="row" gap={1}>
-            <text fg="cyan">Build</text>
-            <text attributes={TextAttributes.DIM} fg="gray"> 
-                &gt; 
-            </text>
-            <text>opus-4.6</text>
-        </box>
-    );
+  return (
+    <box flexDirection="row" gap={1} justifyContent="space-between">
+      <text fg="gray" attributes={TextAttributes.DIM}>
+        Shift + Tab to change mode
+      </text>
+      <box flexDirection="row" gap={1}>
+        <text fg="cyan">Build</text>
+        <text attributes={TextAttributes.DIM} fg="gray">
+          &gt;
+        </text>
+        <text>opus-4.6</text>
+      </box>
+    </box>
+  );
 }
